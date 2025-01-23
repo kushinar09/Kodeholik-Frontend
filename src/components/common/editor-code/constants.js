b07fd2167b0d50748c7e22fba329c83ea4bcdf4c
kushinar09 <phongpd109@gmail.com>
2025-01-23 23:09:11 +0700

    Basic Java Editor: format, custom autocomplete

---
+++ src/components/common/editor-code/constants.js
@@ -0,0 +1,59 @@
+export const INITIAL_CODE = `public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    int[] arrs = new int[5];
+  }
+}`
+
+
+export const JAVA_KEYWORDS = [
+  "abstract",
+  "assert",
+  "boolean",
+  "break",
+  "byte",
+  "case",
+  "catch",
+  "char",
+  "class",
+  "const",
+  "continue",
+  "default",
+  "do",
+  "double",
+  "else",
+  "enum",
+  "extends",
+  "final",
+  "finally",
+  "float",
+  "for",
+  "if",
+  "implements",
+  "import",
+  "instanceof",
+  "int",
+  "interface",
+  "long",
+  "native",
+  "new",
+  "package",
+  "private",
+  "protected",
+  "public",
+  "return",
+  "short",
+  "static",
+  "strictfp",
+  "super",
+  "switch",
+  "synchronized",
+  "this",
+  "throw",
+  "throws",
+  "transient",
+  "try",
+  "void",
+  "volatile",
+  "while"
+]
