958e1ba6b7431d83959c7ab13804300713eac413
Kusuruna <142042717+Kusuruna@users.noreply.github.com>
2025-02-24 21:57:32 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,25 +9,44 @@ import MarkdownEditor from "./components/common/markdown/MarkdownEditor"
 import CodeEditor from "./components/common/editor-code/CodeEditor"
 import ProblemPage from "./components/pages/problems/ProblemPage"
 import HomePage from "./components/pages/HomePage"
+import Login from "./components/pages/authentications/login"
+import ForgotPassword from "./components/pages/authentications/Forgot"
+import ResetPassword from "./components/pages/authentications/reset"
+import { Toaster } from "./components/ui/toaster"
+import ProblemDetail from "./components/pages/problems/ProblemDetail/ProblemDetail"
+import { AuthProvider } from "./context/AuthProvider"
+
+
 
 
 function App() {
   return (
-    <Router>
-      <div className="mx-auto">
-        <Routes>
-          <Route path="/" element={<ProblemPage />} />
-          <Route path="/problems" element={<ProblemPage />} />
-          <Route path="/create" element={<CreateProblem />} />
-          <Route path="/edit/:id" element={<EditProblem />} />
-          <Route path="/markdown" element={<MarkdownEditor />} />
-          <Route path="/template" element={<HomePage />} />
-          <Route path="/courses" element={<ViewCoursePage />} />
-          <Route path="/courses/:id" element={<ViewCourseDetail />} />
-          <Route path="/learn" element={<Learn />} />
-        </Routes>
-      </div>
-    </Router>
+    <>
+
+      <Router>
+        <AuthProvider>
+          <div className="mx-auto">
+            <Routes>
+              <Route path="/" element={<ProblemPage />} />
+              <Route path="/problems" element={<ProblemPage />} />
+              <Route path="/problem/:id" element={<ProblemDetail />} />
+              <Route path="/create" element={<CreateProblem />} />
+              <Route path="/edit/:id" element={<EditProblem />} />
+              <Route path="/markdown" element={<MarkdownEditor />} />
+              <Route path="/template" element={<HomePage />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/forgot" element={<ForgotPassword />} />
+              <Route path="/reset" element={<ResetPassword />} />
+              <Route path="/courses" element={<ViewCoursePage />} />
+              <Route path="/courses/:id" element={<ViewCourseDetail />} />
+              <Route path="/learn" element={<Learn />} />
+            </Routes>
+          </div>
+        </AuthProvider>
+      </Router>
+      <Toaster />
+
+    </>
   )
 }
 
